c89e6402d2c340b11854776eedeb58cdd966a6f4
LiyangSong <song.liy@northeastern.edu>
2023-10-07 19:14:12 -0400

    add correlation related functions

--- eda_utils.py
+++ eda_utils.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 import missingno as msno
 import scipy.stats as stats
 import numpy as np
+import seaborn as sns
+import time
+import statsmodels.api as sm
+from sklearn.preprocessing import StandardScaler
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 
 def split_nominal_and_numerical(a_df: pd.DataFrame, a_target_attr_list: list) -> (list, list):
@@ -28,23 +33,6 @@ def check_for_duplicate_observations(a_df: pd.DataFrame) -> None:
         print('caution: data set contains duplicate observations!!!')
     else:
         print('no duplicate observations observed in data set')
-
-
-def get_flattened_corr_matrix(corr_df: pd.DataFrame, corr_threshold: float = 0.75) -> pd.DataFrame:
-    corr_df = corr_df.where(np.tril(np.ones(corr_df.shape)).astype(np.bool_))
-    flat_attr_correlations_df = corr_df.stack().reset_index()
-    flat_attr_correlations_df = flat_attr_correlations_df.rename(
-        columns={'level_0': 'attribute_x', 'level_1': 'attribute_y', 0: 'correlation'})
-    flat_attr_correlations_df = (flat_attr_correlations_df[
-                                     (flat_attr_correlations_df.correlation != 1) &
-                                     (flat_attr_correlations_df.correlation.abs() > corr_threshold)]
-                                 .sort_values('correlation')
-                                 .reset_index(drop=True))
-
-    print('\n', f'{flat_attr_correlations_df.shape[0]} pairs of attributes with correlations row > {corr_threshold}')
-    print(flat_attr_correlations_df)
-
-    return flat_attr_correlations_df
 
 
 def check_out_missingness(a_df: pd.DataFrame, sample_size_threshold: int = 250, verbose: bool = True,
@@ -91,3 +79,102 @@ def check_out_target_distribution(a_df: pd.DataFrame, a_target_attr: list) -> No
     statistic, p_value = stats.normaltest(a_df[a_target_attr].dropna())
     print('\ntest data for normality:\n')
     print(f'\nnull hypothesis: data comes from a normal distribution - p_value: {p_value}')
+
+
+# check correlations
+
+def get_flattened_corr_matrix(corr_df: pd.DataFrame, corr_threshold: float = 0.75) -> pd.DataFrame:
+    corr_df = corr_df.where(np.tril(np.ones(corr_df.shape)).astype(np.bool_))
+    flat_attr_correlations_df = corr_df.stack().reset_index()
+    flat_attr_correlations_df = flat_attr_correlations_df.rename(
+        columns={'level_0': 'attribute_x', 'level_1': 'attribute_y', 0: 'correlation'})
+    flat_attr_correlations_df = (flat_attr_correlations_df[
+                                     (flat_attr_correlations_df.correlation != 1) &
+                                     (flat_attr_correlations_df.correlation.abs() > corr_threshold)]
+                                 .sort_values('correlation')
+                                 .reset_index(drop=True))
+
+    print('\n', f'{flat_attr_correlations_df.shape[0]} pairs of attributes with correlations row > {corr_threshold}')
+    print(flat_attr_correlations_df)
+
+    return flat_attr_correlations_df
+
+
+def get_correlation_data_frame(a_df: pd.DataFrame, a_num_attr_list: list, method: str = 'pearson',
+                               corr_threshold: float = 0.75) -> pd.DataFrame:
+    print(f'\ncorrelation data frame using {method} method with threshold {corr_threshold}:\n')
+
+    attr_correlations_df = a_df[a_num_attr_list].corr(method=method)
+    flat_attr_correlations_df = get_flattened_corr_matrix(attr_correlations_df, corr_threshold=corr_threshold)
+
+    return flat_attr_correlations_df
+
+
+def print_corr_of_num_attrs(a_df: pd.DataFrame, a_num_attr_list: list, method: str = 'pearson',
+                            corr_threshold: float = 0.50) -> None:
+    print('heatmap of design matrix attribute correlations:\n')
+
+    if a_df[a_num_attr_list].shape[1] < 20:
+        fig, ax = plt.subplots(figsize=(15, 15))
+        sns.heatmap(a_df[a_num_attr_list].corr(method=method), annot=True, ax=ax)
+        plt.show()
+        _ = get_correlation_data_frame(a_df, a_num_attr_list, method=method, corr_threshold=corr_threshold)
+    else:
+        print(f'\nSkip correlation heat map - {a_df[a_num_attr_list].shape[1]} attributes is too many for a useful '
+              f'visual output.', sep='')
+        _ = get_correlation_data_frame(a_df, a_num_attr_list, method=method, corr_threshold=corr_threshold)
+
+
+def drop_obs_with_nans(a_df: pd.DataFrame) -> pd.DataFrame:
+    if a_df.isna().sum().sum() > 0:
+        print(f'\nfound observations with nans - pre obs. drop a_df.shape: {a_df.shape}')
+        a_df = a_df.dropna(axis=0, how='any')
+        print(f'post obs. drop a_df.shape: {a_df.shape}')
+
+    return a_df
+
+
+def prep_data_for_vif_calc(a_df: pd.DataFrame, a_num_attr_list: list) -> (pd.DataFrame, str):
+    # drop observations with nans
+    a_df = drop_obs_with_nans(a_df[a_num_attr_list])
+
+    # prepare the data - make sure you perform the analysis on the design matrix
+    design_matrix = None
+    bias_attr = None
+    for attr in a_df[a_num_attr_list]:
+        if a_df[attr].nunique() == 1 and a_df[attr].iloc[0] == 1:  # found the bias attribute
+            design_matrix = a_df[a_num_attr_list]
+            bias_attr = attr
+            print('found the bias term - no need to add one')
+            break
+
+    if design_matrix is None:
+        design_matrix = sm.add_constant(a_df[a_num_attr_list])
+        bias_attr = 'const'
+        a_num_attr_list = [bias_attr] + a_num_attr_list
+        print('\nAdded a bias term to the data frame to construct the design matrix for assessment of vifs.', sep='')
+
+    # if numerical attributes in the data frame are not scaled then scale them - don't scale the bias term
+    a_num_attr_list.remove(bias_attr)
+    if not (a_df[a_num_attr_list].mean() <= 1e-14).all():
+        print('scale the attributes - but not the bias term')
+        design_matrix[a_num_attr_list] = StandardScaler().fit_transform(design_matrix[a_num_attr_list])
+
+    return design_matrix, bias_attr
+
+
+def print_vifs(a_df: pd.DataFrame, a_num_attr_list: list) -> pd.DataFrame:
+    print('investigate multi co-linearity: calculate variance inflation factors:\n')
+
+    design_matrix, bias_attr = prep_data_for_vif_calc(a_df, a_num_attr_list)
+
+    # calculate the vifs
+    vif_df = pd.DataFrame()
+    vif_df['attribute'] = design_matrix.columns.tolist()
+    vif_df['vif'] = [variance_inflation_factor(design_matrix.values, i) for i in range(design_matrix.shape[1])]
+    vif_df['vif'] = vif_df['vif'].round(2)
+
+    print('\n', vif_df, sep='')
+    time.sleep(2)
+
+    return vif_df
